c7868452cb7244ceda457a8d61244e1131e2258f
Adam Rackis <arackis@centralreach.com>
2017-09-27 23:47:03 -0500

    circular dep repro

---
+++ src/a.js
@@ -0,0 +1,5 @@
+import b from "./b";
+
+export default {
+  value: b.value
+};
--- src/module.js
+++ src/module.js
@@ -1,52 +1,5 @@
-import createGraphqlSchema from "./createGraphqlSchema/createSchema";
-export { createGraphqlSchema };
+import a from "./a";
+import b from "./b";
 
-import { graphql } from "graphql";
-import { makeExecutableSchema } from "graphql-tools";
-
-import * as graphqlAst from "./queryUtilities/parseAst";
-export { graphqlAst };
-
-import * as dataTypes from "./createGraphqlSchema/dataTypes";
-export { dataTypes };
-
-let astPassedIn, schema;
-
-let typeDefs = `
-    type Author {
-      _id: String
-      name: String
-    }
-    type Book {
-      _id: String
-      title: String
-      publisher: String
-      isbn: String
-      pages: Int
-      weight: Float
-      author: Author
-    }
-    type Query {
-      allBooks: [Book]
-    }
-  `;
-let resolvers = {
-  Query: {
-    allBooks(root, args, context, ast) {
-      debugger;
-      let fieldNode = ast.fieldNodes.find(fn => fn.kind == "Field");
-      if (fieldNode) {
-        let primitives = fieldNode.selectionSet.selections.filter(sel => sel.selectionSet == null).map(sel => sel.name.value);
-        let objectSelections = fieldNode.selectionSet.selections.filter(sel => sel.selectionSet == null).map(sel => sel.name.value);
-
-        return { primitives, objectSelections };
-      }
-      astPassedIn = ast;
-      return [];
-    }
-  }
-};
-
-schema = makeExecutableSchema({ typeDefs, resolvers });
-
-graphql(schema, "{allBooks{_id, title, publisher, author { _id, name }}}");
+console.log(a.value);
+console.log(b.value);
